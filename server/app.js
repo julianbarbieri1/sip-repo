16a8af9709fcfd00b6ea9c5399b684be87484767
julianbarbieri1 <julian.barbieri@nybblegroup.com>
2023-08-30 18:28:40 -0300

    Sprint 1

--- server/app.js
+++ server/app.js
@@ -1,19 +1,34 @@
 import express from "express";
-import { crearCliente, getMarketListCliente, getClienteById, getAllClientes, crearAdministrador, getAllAdministradores } from "./database.js";
+import execute from 'mysql2/promise';
+import { obtenerNombreCompletoDelUsuarioAdmin, iniciarSesionAdmin, verificarAdministradores, verificarUsuario, obtenerNombreCompletoDelUsuario, getAllAdministradores, crearAdministrador, getAllClientes, crearCliente, iniciarSesion } from "./database.js";
 import cors from 'cors';
-import bcrypt from 'bcrypt';
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
 
+const pool = mysql
+.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+})
+.promise();
 
 const corsOption = {
-    origin: "http://localhost:3000", //especifica origen permitido
+    //origin: "http://localhost:8081", //especifica origen permitido en REACT NATIVE
+    origin: "http://localhost:3000", //especifica origen permitido en NEXT JS
     methods: ["GET", "POST"], //especifica los metodos permitidos
     credentials: true, //permite enviar las credenciales (cookies, autenticacion)
 }
 const app = express();
 app.use(express.json());
 app.use(cors(corsOption));
-app.use(express.urlencoded({ extended: false}))
 
+
+//app.use(bodyParser.urlencoded({extended: true}));
+
+/*
 app.get("/usuarios/:id", async(req,res) =>{
     const user = await getClienteById(req.params.id);
     res.status(200).send(user);
@@ -29,49 +44,128 @@ app.get("/usuarios", async (req, res) => {
     }
   });
 
-app.post("/registrarseCliente", async(req,res) => {
-    const {nombre, usuario, contraseña} = req.user;
-    const user = await crearCliente(nombre, usuario, contraseña);
-    res.status(201).send(user);
-});
 
-//administradores
+*/
 
-app.post("/registrarseAdmin", async(req,res) => {
-  
+//CLIENTES
+
+app.get("/", async (req, res) => {
   try {
-    //bcrypt srive para encriptar los datos de los usuarios
-    //const hashPassword = bcrypt(req.body.contraseña, 10);
-    const data = {
-      nombre: req.body.nombre, 
-      usuario: req.body.usuario, 
-      contraseña: req.body.contraseña
-    };
-    const user = await crearAdministrador(data);
-    res.status(201).send(user);
-    res.redirect("/");
-    console.log(data);
+    const users = await getAllClientes();
+    res.status(200).json(users); // Cambiado send por json para enviar los datos en formato JSON
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ error: "Ha ocurrido un error en el servidor." });
   }
-  
+});
+app.post("/registrarseCliente", async (req, res) => {
+  const nombre_completo = req.body.nombre_completo;
+  const nombre_usuario = req.body.nombre_usuario;
+  const contraseña = req.body.contraseña;
+  try {
+    const user = await crearCliente(nombre_completo, nombre_usuario, contraseña);
+    res.status(201).json(user);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Ha ocurrido un error en el servidor." });
+  }
 });
 
-app.post("/registrarseAdmin", async(req,res) => {
-  req.body.nombre_completo;
+app.post("/iniciarSesion", async (req, res) => {
+  const { nombre_usuario, contraseña } = req.body;
+
+  try {
+    const user = await iniciarSesion(nombre_usuario, contraseña);
+
+    if (user) {
+      const nombreCompletoDelUsuario = obtenerNombreCompletoDelUsuario(nombre_usuario);
+      console.log(nombre_usuario);
+      res.status(200).json({ nombre_completo: nombreCompletoDelUsuario });
+      //res.status(200).json({ mensaje: "Inicio de sesión exitoso" });
+    } else {
+      res.status(401).json({ mensaje: "Credenciales incorrectas" });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Ha ocurrido un error en el servidor." });
+  }
 });
 
-app.post("/registrarseAdmin", async(req,res) => {
-  req.body.nombre_completo;
+app.post("/verificarUsuario", async (req, res) => {
+  const { nombre_usuario } = req.body;
+  try {
+    const user = await verificarUsuario(nombre_usuario);
+    if (user) {
+      res.status(200).json({ exists: true }); // Si el usuario existe
+    } else {
+      res.status(200).json({ exists: false }); // Si el usuario no existe
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Ha ocurrido un error en el servidor." });
+  }
+});
+
+//ADMINISTRADORES
+
+app.post("/verificarAdministradores", async (req, res) => {
+  const { nombre_usuario } = req.body;
+  try {
+    const user = await verificarAdministradores(nombre_usuario);
+    if (user) {
+      res.status(200).json({ exists: true }); // Si el usuario existe
+    } else {
+      res.status(200).json({ exists: false }); // Si el usuario no existe
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Ha ocurrido un error en el servidor." });
+  }
 });
 
 app.get("/administradores", async (req, res) => {
+  try {
     const users = await getAllAdministradores();
     res.status(200).json(users); // Cambiado send por json para enviar los datos en formato JSON
-    console.log(users);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Ha ocurrido un error en el servidor." });
+  }
 });
 
+app.post("/registrarseAdmin", async (req, res) => {
+  const nombre_completo = req.body.nombre_completo;
+  const nombre_usuario = req.body.nombre_usuario;
+  const contraseña = req.body.contraseña;
+  
+  try {
+    const user = await crearAdministrador(nombre_completo, nombre_usuario, contraseña);
+    res.status(201).json(user);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Ha ocurrido un error en el servidor." });
+  }
+});
+
+app.post("/iniciarSesionAdmin", async (req, res) => {
+  const { nombre_usuario, contraseña } = req.body;
+
+  try {
+    const user = await iniciarSesionAdmin(nombre_usuario, contraseña);
+
+    if (user) {
+      const nombreCompletoDelUsuario = obtenerNombreCompletoDelUsuarioAdmin(nombre_usuario);
+      console.log(nombre_usuario);
+      res.status(200).json({ nombre_completo: nombreCompletoDelUsuario });
+      //res.status(200).json({ mensaje: "Inicio de sesión exitoso" });
+    } else {
+      res.status(401).json({ mensaje: "Credenciales incorrectas" });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Ha ocurrido un error en el servidor." });
+  }
+});
 
 app.listen(8080, () => {
     console.log("Server running on port 8080");
--- server/database.js
+++ server/database.js
@@ -19,12 +19,12 @@ export async function getMarketListCliente(usuario, contraseña){
     return row;
 }
 
-export async function crearCliente(nombre, usuario, contraseña){
+export async function crearCliente(nombre_completo, nombre_usuario, contraseña){
     const [user] = await pool.query(
         'INSERT INTO clientes (nombre_completo, nombre_usuario, contraseña) VALUES (?,?,?)',
-        [nombre,usuario,contraseña]   
+        [nombre_completo,nombre_usuario,contraseña]   
     )
-    return [user];
+    return user;
 }
 
 export async function getClienteById(id){
@@ -40,19 +40,64 @@ export async function getAllClientes() {
     return rows;
 }
 
+export async function iniciarSesion(nombre_usuario, contraseña) {
+    const [row] = await pool.query(
+      'SELECT * FROM clientes WHERE nombre_usuario = ? AND contraseña = ?',
+      [nombre_usuario, contraseña]
+    );
+    return row.length > 0 ? row[0] : null;
+  }
+export async function obtenerNombreCompletoDelUsuario(nombre_usuario, contraseña) {
+  const nombre_comleto = await pool.query(
+    'SELECT nombre_completo FROM clientes WHERE nombre_usuario = ? AND contraseña = ?',
+    [nombre_usuario, contraseña]
+  );
+  return nombre_comleto;
+}    
+
+export async function verificarUsuario(nombre_usuario) {
+  const [row] = await pool.query(
+    'SELECT * FROM clientes WHERE nombre_usuario = ?',
+    [nombre_usuario]
+  );
+  return row[0]; // Retorna el usuario si existe, o undefined si no existe
+}
+
 //ADMINISTRADORES
 
-export async function crearAdministrador(nombre, usuario, contraseña){
-    const user = await pool.query(
-        'INSERT INTO administradores VALUES (?,?,?)',
-        [nombre,usuario,contraseña]   
-    );
-    return user;
+export async function verificarAdministradores(nombre_usuario) {
+  const [row] = await pool.query(
+    'SELECT * FROM administradores WHERE nombre_usuario = ?',
+    [nombre_usuario]
+  );
+  return row[0]; // Retorna el usuario si existe, o undefined si no existe
 }
-
 
 export async function getAllAdministradores() {
-    const [rows] = await pool.query('SELECT * FROM administradores');
-    return rows;
+  const [rows] = await pool.query('SELECT * FROM administradores');
+  return rows;
 }
 
+export async function crearAdministrador(nombre_completo, nombre_usuario, contraseña) {
+  const [user] = await pool.query(
+    'INSERT INTO administradores (nombre_completo, nombre_usuario, contraseña) VALUES (?, ?, ?)',
+    [nombre_completo, nombre_usuario, contraseña]
+  );
+  return user;
+}
+
+export async function obtenerNombreCompletoDelUsuarioAdmin(nombre_usuario, contraseña) {
+  const nombre_comleto = await pool.query(
+    'SELECT nombre_completo FROM administradores WHERE nombre_usuario = ? AND contraseña = ?',
+    [nombre_usuario, contraseña]
+  );
+  return nombre_comleto;
+} 
+
+export async function iniciarSesionAdmin(nombre_usuario, contraseña) {
+  const [row] = await pool.query(
+    'SELECT * FROM administradores WHERE nombre_usuario = ? AND contraseña = ?',
+    [nombre_usuario, contraseña]
+  );
+  return row.length > 0 ? row[0] : null;
+}
